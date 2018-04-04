fc6c68209edc5b2a52b1e33cdcce73c656aa1fce
Zoltán Szabó <zoltan.szabo@atolldev.hu>
2018-04-04 08:44:04 +0200

    Initial commit Add Spring Boot backend and Angular frontend

---
+++ src/main/java/com/szabodev/test/google/GoogleLoginTestApplication.java
@@ -0,0 +1,12 @@
+package com.szabodev.test.google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleLoginTestApplication.class, args);
+    }
+}
